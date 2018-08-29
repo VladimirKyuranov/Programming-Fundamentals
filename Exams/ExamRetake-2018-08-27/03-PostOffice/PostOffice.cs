0c4b86371c6944cfcfa20dbb6c9c11028e86b8ac
Vladimir Kyuranov <the_crow@abv.bg>
2018-08-29 20:41:23 +0300

    edits

--- Exams/ExamRetake-2018-08-27/03-PostOffice/PostOffice.cs
+++ Exams/ExamRetake-2018-08-27/03-PostOffice/PostOffice.cs
@@ -11,7 +11,6 @@ class PostOffice
             .Split('|', StringSplitOptions.RemoveEmptyEntries);
         string capitalsPattern = @"(?<symbol>[$#%*&])(?<capitals>[A-Z]+)\k<symbol>";
         string lengthsPattern = @"(?<capital>6[5-9]|[78][0-9]|90):(?<length>\d{2})";
-
         char[] capitals = Regex.Match(input[0], capitalsPattern).Groups["capitals"].Value.ToCharArray();
         MatchCollection lengths = Regex.Matches(input[1], lengthsPattern);
         string[] words = input[2].Split();
@@ -23,17 +22,15 @@ class PostOffice
                 .FirstOrDefault(l => (char)int.Parse(l.Groups["capital"].Value) == capital)
                 .Groups["length"].Value);
             string wordPattern = $@"^{capital}\S{{{length}}}$";
+
             foreach (string word in words)
             {
                 if (Regex.IsMatch(word, wordPattern))
                 {
-                    result.Add(word);
+                    Console.WriteLine(word);
                     break;
                 }
             }
         }
-
-        result = result.Distinct().ToList();
-        Console.WriteLine(string.Join(Environment.NewLine, result));
     }
 }
