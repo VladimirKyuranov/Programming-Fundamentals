00268b68fcae16ce27f8d43bb89f509de4997ed1
Vladimir Kyuranov <the_crow@abv.bg>
2018-08-29 10:16:23 +0300

    +

--- Exams/ExamRetake-2018-08-27/03-PostOffice/PostOffice.cs
+++ Exams/ExamRetake-2018-08-27/03-PostOffice/PostOffice.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 class PostOffice
 {
@@ -6,7 +8,32 @@ class PostOffice
     {
         string[] input = Console.ReadLine()
             .Split('|', StringSplitOptions.RemoveEmptyEntries);
-        string capitalsPattern = @"(?<symbol>[$])(?<capitals>[A-Z]+)\k<symbol>";
-        string firstLetterAndLengthPattern = @"(?<firstLetter>[65-90]):(?<length>\d{2})";
+        string capitalsPattern = @"(?<symbol>[$#%*&])(?<capitals>[A-Z]+)\k<symbol>";
+        string firstLetterAndLengthPattern = @"(?<capital>6[5-9]|[78][0-9]|90):(?<length>\d{2})";
+
+        string capitals = Regex.Match(input[0], capitalsPattern).Groups["capitals"].Value;
+        MatchCollection capitalsAndLengthMatches = Regex.Matches(input[1], firstLetterAndLengthPattern);
+        string[] words = input[2].Split();
+        List<string> result = new List<string>();
+
+        foreach (string word in words)
+        {
+            foreach (Match match in capitalsAndLengthMatches)
+            {
+                char capital = (char)int.Parse(match.Groups["capital"].Value);
+                int length = int.Parse(match.Groups["length"].Value);
+                if (capitals.Contains(capital))
+                {
+                    string wordPattern = $@"^{capital}[^ ]{{{length}}}$";
+                    if (Regex.IsMatch(word, wordPattern))
+                    {
+                        result.Add(word);
+                        break;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine(string.Join(Environment.NewLine, result));
     }
 }
